1334866c60c8b962d9b4092d111caf68ccfe9e5e
Ahmed kallel <113143693+ahmede9ka@users.noreply.github.com>
2024-11-23 00:51:34 +0100

    login page

--- Frontend/project/src/app/views/client/login/login.component.ts
+++ Frontend/project/src/app/views/client/login/login.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatInputModule,
+    MatButtonModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  usernameFormGroup!: FormGroup;
+  passwordFormGroup!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // Initialize Form Groups
+    this.usernameFormGroup = this.fb.group({
+      username: ['', Validators.required],
+    });
+
+    this.passwordFormGroup = this.fb.group({
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.usernameFormGroup.valid && this.passwordFormGroup.valid) {
+      const username = this.usernameFormGroup.get('username')?.value;
+      const password = this.passwordFormGroup.get('password')?.value;
+      console.log('Username:', username);
+      console.log('Password:', password);
+
+      // Perform login logic here
+      alert('Login Successful!');
+    } else {
+      alert('Please complete all steps.');
+    }
+  }
 }
--- Frontend/project/src/app/views/client/login/login.module.ts
+++ Frontend/project/src/app/views/client/login/login.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { LoginRoutingModule } from './login-routing.module';
-
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    LoginRoutingModule
+    
+    LoginRoutingModule,
+    
   ]
 })
 export class LoginModule { }
