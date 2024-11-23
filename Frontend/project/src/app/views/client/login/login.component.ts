f541d854867abc5533ce2d616ff8cac66f7d3f1d
Maher-Khemakhem <khemakhemmaher2003@gmail.com>
2024-11-23 14:46:24 +0100

    front commit

--- Frontend/project/src/app/views/client/login/login.component.ts
+++ Frontend/project/src/app/views/client/login/login.component.ts
@@ -1,50 +1,55 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import { MatStepperModule } from '@angular/material/stepper';
-
-
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,
-      MatStepperModule,
-      MatButtonModule,
-      ReactiveFormsModule,
-      MatAutocompleteModule,
-      MatIconModule,
-      
-      MatFormFieldModule
-    ],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent {
+  private _formBuilder = inject(FormBuilder);
+
+  // Define the form groups and form controls
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
   isLinear = true;
-  firstFormGroup!: FormGroup;
-  secondFormGroup!: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) {}
-
-  ngOnInit() {
+  constructor() {
+    // Initialize the form groups with controls
     this.firstFormGroup = this._formBuilder.group({
-      firstName: ['', Validators.required],
+      firstName: ['', Validators.required] // This should match the formControlName in the template
     });
+
     this.secondFormGroup = this._formBuilder.group({
-      lastName: ['', Validators.required],
+      lastName: ['', Validators.required] // This should match the formControlName in the template
     });
   }
 
+  // onSubmit method, to handle the form submission
   onSubmit() {
-    console.log('Form submitted:', {
-      firstName: this.firstFormGroup.value.firstName,
-      lastName: this.secondFormGroup.value.lastName,
-    });
+    if (this.firstFormGroup.valid && this.secondFormGroup.valid) {
+      console.log('Form Submitted', this.firstFormGroup.value, this.secondFormGroup.value);
+    }
   }
 }
--- Frontend/project/src/app/views/client/login/login.module.ts
+++ Frontend/project/src/app/views/client/login/login.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
-import { LoginRoutingModule } from './login-routing.module';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
-    
-    LoginRoutingModule,
-    
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
-export class LoginModule { }
+export class LoginModule {}
