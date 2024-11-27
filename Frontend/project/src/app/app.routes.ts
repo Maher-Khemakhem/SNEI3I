dc2a2d26a174c14de447b5c70f8c3f9f538db296
fourat <fouratmarouen0@gmail.com>
2024-11-27 09:54:06 +0100

    auth guard added

--- Frontend/project/src/app/app.routes.ts
+++ Frontend/project/src/app/app.routes.ts
@@ -4,23 +4,30 @@ import { LoginComponent } from './views/client/login/login.component';
 import { SignUpWorkerComponent } from './views/worker/sign-up-worker/sign-up-worker.component';
 import { LogInClientComponent } from './views/client/log-in-client/log-in-client.component';
 import { LogInWorkerComponent } from './views/worker/log-in-worker/log-in-worker.component';
+import { authGuard } from './services/auth.guard';
 
 
 export const routes: Routes = [
     
     {
-        path:'',component:ClientLayoutComponent,children:[
+        path:'',
+        component:ClientLayoutComponent,
+        children:[
             
         ]
     },
     { 
-        path: 'login', component: LoginComponent 
+        path: 'login', component: LoginComponent, 
     },
     { 
         path: 'signupworker', component: SignUpWorkerComponent 
     },
     { 
-        path: 'loginclient', component: LogInClientComponent 
+        path: 'loginclient', component: LogInClientComponent ,
+        children:[],
+        canActivate:[authGuard],
+        
+
     },
     { 
         path: 'loginworker', component: LogInWorkerComponent 
