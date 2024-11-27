5e4b52a421de927df1f3a41c1f4898d1094f5021
Maher-Khemakhem <khemakhemmaher2003@gmail.com>
2024-11-27 10:17:58 +0100

    maherr

--- Frontend/project/src/app/services/auth.guard.ts
+++ Frontend/project/src/app/services/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
-<<<<<<< HEAD
 @Injectable({
   providedIn: 'root',
 })
@@ -20,15 +19,5 @@ export class authGuard implements CanActivate {
 
     // Allow access if the user is not logged in
     return true;
-=======
-export const authGuard: CanActivateFn = (route, state) => {
-  const localUser = localStorage.getItem('user_id');
-  const router = inject(Router);
-  if(localUser != null){
-    return true
-  }else{
-    router.navigateByUrl('/');
-    return false;
->>>>>>> edd4111d284d43d360996c1934a3536c2fbc3fe0
   }
 }
