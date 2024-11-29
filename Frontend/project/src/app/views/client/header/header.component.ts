7faefced0d1709bf51663586dfaba5f23624c138
Ahmed kallel <113143693+ahmede9ka@users.noreply.github.com>
2024-11-29 16:29:01 +0100

    header

--- Frontend/project/src/app/views/client/header/header.component.ts
+++ Frontend/project/src/app/views/client/header/header.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  MatIconModule,MatSelectModule,MatOptionModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+  locations = ['New York', 'San Francisco', 'Chicago', 'Los Angeles'];
+  specialties = ['Cardiology', 'Dermatology', 'Pediatrics', 'Neurology'];
 
+  selectedLocation: string | undefined;
+  selectedSpecialty: string | undefined;
+
+  search() {
+    console.log('Searching for:', {
+      location: this.selectedLocation,
+      specialty: this.selectedSpecialty,
+    });
+  }
 }
--- Frontend/project/src/app/views/client/header/header.module.ts
+++ Frontend/project/src/app/views/client/header/header.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderRoutingModule } from './header-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HeaderRoutingModule
+    HeaderRoutingModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class HeaderModule { }
