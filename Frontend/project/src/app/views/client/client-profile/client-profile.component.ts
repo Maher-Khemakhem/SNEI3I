f4851dd0506621ec17f0f4f12ac77e0672ace91b
Ahmed kallel <113143693+ahmede9ka@users.noreply.github.com>
2024-12-02 15:55:01 +0100

    profile

--- Frontend/project/src/app/views/client/client-profile/client-profile.component.ts
+++ Frontend/project/src/app/views/client/client-profile/client-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../../services/client.service';
 
 @Component({
   selector: 'app-client-profile',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './client-profile.component.html',
   styleUrl: './client-profile.component.css'
 })
-export class ClientProfileComponent {
+export class ClientProfileComponent implements OnInit {
+  constructor(private clientservice:ClientService) {}
+  client : any;
+  ngOnInit(): void {
+    // Get the worker ID from localStorage
+    
+    const id = localStorage.getItem('landingpageclientid');  // Corrected syntax
+    if (id) {
+      // Ensure the ID exists before making a request
+      this.clientservice.getClientById('674da09fd806f0b21be21e0a').subscribe({
+       
+        next: (clientdata) => {
+          this.client = clientdata;
+          
+          
+         },
+        error: (err) => {
+          console.error('Error fetching client data:', err); // Handle errors gracefully
+        }
+      });
+    } else {
+      console.error('No client ID found in localStorage'); // Log error if ID is not found
+    }
+  }
+
 
 }
