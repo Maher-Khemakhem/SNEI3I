4e09c4efe99bec57972f65c94e78262c3b8c8a35
Maher-Khemakhem <khemakhemmaher2003@gmail.com>
2024-12-03 13:48:30 +0100

    7anichon

--- Frontend/project/src/app/services/auth.guard.ts
+++ Frontend/project/src/app/services/auth.guard.ts
@@ -7,32 +7,28 @@ import {jwtDecode} from 'jwt-decode';
 export class authGuard implements CanActivate {
   constructor(private router: Router) {}
 
-  canActivate(): Promise<boolean> {
-    return new Promise((resolve) => {
-      // Replace this with your actual authentication check
-      const token = localStorage.getItem('token'); // Example: check for a token
+  canActivate(): boolean {
+    // Replace this with your actual authentication check
+    const token = localStorage.getItem('token'); // Example: check for a token
 
-      if (token) {
-        try {
-          // Decode and validate token
-          const decodedToken: any = jwtDecode(token);
+    if (token) {
+      try {
+        // Decode and validate token
+        const decodedToken: any = jwtDecode(token);
   
-          // Check if the token is expired
-          const isExpired = decodedToken.exp * 1000 < Date.now();
-          if (!isExpired) {
-            this.router.navigate(['/']);
-            console.log('redirected');
-            resolve(false); // Deny access if token is expired
-          } else {
-            resolve(true); // Allow access if token is valid
-          }
-        } catch (error) {
-          console.error('Invalid token:', error);
-          resolve(false); // Deny access if token is invalid
+        // Check if the token is expired
+        const isExpired = decodedToken.exp * 1000 < Date.now();
+        if (!isExpired) {
+          this.router.navigate(['/']);
+          console.log('redirected');
+          return false;
         }
-      } else {
-        resolve(false); // Deny access if there is no token
+      } catch (error) {
+        console.error('Invalid token:', error);
       }
-    });
+      
+    }
+    console.log('not redirected');
+    return true;
   }
 }
